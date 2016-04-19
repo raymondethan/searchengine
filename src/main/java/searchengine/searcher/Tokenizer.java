fbe394dbb941af8f2f14dfa994190e4580735076
Jay Harris <jay.harris@outlook.co.nz>
2016-04-19 10:53:14 +0800

    Adds a method to the tokenizer for getting tokens

--- src/main/java/searchengine/searcher/Tokenizer.java
+++ src/main/java/searchengine/searcher/Tokenizer.java
@@ -1,7 +1,16 @@
 package searchengine.searcher;
 
+import java.util.List;
+
 /**
  *
  */
 public class Tokenizer {
+    public Tokenizer() {
+
+    }
+
+    public List<String> getTokens(String query) {
+        return null;
+    }
 }
