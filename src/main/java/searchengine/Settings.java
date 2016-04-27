512727fdcca73f933bdaa31d47eaad51567aac48
Jay Harris <jay.harris@outlook.co.nz>
2016-04-27 17:52:11 +0800

    Changes the default number of pages to crawl

--- src/main/java/searchengine/Settings.java
+++ src/main/java/searchengine/Settings.java
@@ -5,7 +5,7 @@ package searchengine;
  */
 public class Settings {
     public int port = 8080;
-    public int crawlCount = 30;
+    public int crawlCount = 300;
     public boolean shouldCrawl;
     public String startUrl = "http://www.cse.ust.hk/~ericzhao/COMP4321/TestPages/testpage.htm";
 }
