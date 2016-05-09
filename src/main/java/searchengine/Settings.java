059dae548092c0a196189ce2ae8ca4deae19638e
Jay Harris <jay.harris@outlook.co.nz>
2016-05-09 09:14:05 +0800

    Final changes for submission

--- src/main/java/searchengine/Settings.java
+++ src/main/java/searchengine/Settings.java
@@ -4,14 +4,14 @@ package searchengine;
  *
  */
 public class Settings {
+    public final int maxInsertionsBeforeMerge = 75;
+    public final float damping_factor = (float) .85;
     public int port = 8000;
     public int crawlCount = 300;
-    public final int maxInsertionsBeforeMerge = 75;
     public boolean shouldCrawl = true;
     public String startUrl = "http://www.cse.ust.hk/~ericzhao/COMP4321/TestPages/testpage.htm";
     public int maxTermsPrinted = 5;
-    public int maxSearchResults = 75;
+    public int maxSearchResults = 50;
     public double alpha = .9;
     public int pageRankIterations = 40;
-    public final float damping_factor = (float) .85;
 }
